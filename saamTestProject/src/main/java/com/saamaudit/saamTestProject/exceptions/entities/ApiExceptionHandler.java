d6daeee9740976dfe7a07446c0f7721f0a6825a6
Carlos Henrique Mota Martins <107642033+CarlosHMms@users.noreply.github.com>
2025-10-30 14:10:10 -0300

    feat: add exception handlers

--- saamTestProject/src/main/java/com/saamaudit/saamTestProject/exceptions/entities/ApiExceptionHandler.java
+++ saamTestProject/src/main/java/com/saamaudit/saamTestProject/exceptions/entities/ApiExceptionHandler.java
@@ -1,8 +1,11 @@
 package com.saamaudit.saamTestProject.exceptions.entities;
 
 import com.saamaudit.saamTestProject.exceptions.NotFoundException;
+import org.apache.coyote.BadRequestException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -21,6 +24,36 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
                 statusCode,
                 ZonedDateTime.now(ZoneId.of("America/Sao_Paulo"))
 
+        );
+        return new ResponseEntity<>(exceptionPayload, statusCode);
+    }
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<Object> badCredentialsExceptionHandler(BadCredentialsException e){
+        HttpStatus statusCode = HttpStatus.BAD_REQUEST;
+        ExceptionPayload exceptionPayload = new ExceptionPayload(
+                e.getMessage(),
+                statusCode,
+                ZonedDateTime.now(ZoneId.of("America/Sao_Paulo"))
+        );
+        return new ResponseEntity<>(exceptionPayload, statusCode);
+    }
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<Object> badRequestExceptionHandler(BadRequestException e){
+        HttpStatus statusCode = HttpStatus.BAD_REQUEST;
+        ExceptionPayload exceptionPayload = new ExceptionPayload(
+                e.getMessage(),
+                statusCode,
+                ZonedDateTime.now(ZoneId.of("America/Sao_Paulo"))
+        );
+        return new ResponseEntity<>(exceptionPayload, statusCode);
+    }
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<Object> accessDeniedExceptionHandler(AccessDeniedException e){
+        HttpStatus statusCode = HttpStatus.UNAUTHORIZED;
+        ExceptionPayload exceptionPayload = new ExceptionPayload(
+                e.getMessage(),
+                statusCode,
+                ZonedDateTime.now(ZoneId.of("America/Sao_Paulo"))
         );
         return new ResponseEntity<>(exceptionPayload, statusCode);
     }
