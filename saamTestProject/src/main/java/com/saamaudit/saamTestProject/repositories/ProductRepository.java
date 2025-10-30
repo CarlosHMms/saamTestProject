1be8c526d221d9a6739e869f8cdd66f40879f04b
Carlos Henrique Mota Martins <107642033+CarlosHMms@users.noreply.github.com>
2025-10-30 00:25:33 -0300

    feat: add delete method on product repository

--- saamTestProject/src/main/java/com/saamaudit/saamTestProject/repositories/ProductRepository.java
+++ saamTestProject/src/main/java/com/saamaudit/saamTestProject/repositories/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.saamaudit.saamTestProject.repositories;
 
 import com.saamaudit.saamTestProject.entities.Product;
+import com.saamaudit.saamTestProject.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    void deleteByUser(User user);
 }
