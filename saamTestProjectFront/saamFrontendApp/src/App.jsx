24d2c20807b4f6e7fc7c53159190a5b8a209568f
Carlos Henrique Mota Martins <107642033+CarlosHMms@users.noreply.github.com>
2025-10-30 07:51:43 -0300

    feat: add app file router

---
+++ saamTestProjectFront/saamFrontendApp/src/App.jsx
@@ -0,0 +1,41 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { CssBaseline } from "@mui/material";
+import { ThemeProvider } from "@mui/material/styles";
+import LoginPage from "./pages/LoginPage";
+import ProductPage from "./pages/ProductPage";
+import { authService } from "./services/authService";
+import { theme } from "./theme/theme";
+import "./App.css";
+
+const PrivateRoute = ({ children }) => {
+  return authService.isAuthenticated() ? children : <Navigate to="/login" />;
+};
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route
+            path="/products"
+            element={
+              <PrivateRoute>
+                <ProductPage />
+              </PrivateRoute>
+            }
+          />
+          <Route path="/" element={<Navigate to="/products" />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
+  );
+}
+
+export default App;
