134579379524e4a8026be9f526fc02539daed8af
Carlos Henrique Mota Martins <107642033+CarlosHMms@users.noreply.github.com>
2025-10-29 23:58:09 -0300

    feat: create product entity

--- saamTestProject/src/main/java/com/saamaudit/saamTestProject/entities/Product.java
+++ saamTestProject/src/main/java/com/saamaudit/saamTestProject/entities/Product.java
@@ -1,4 +1,82 @@
 package com.saamaudit.saamTestProject.entities;
 
+import jakarta.persistence.*;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "tb_products")
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id")
+    private Long productId;
+
+    @Column(unique = true)
+    private String name;
+
+    private String description;
+    private String price;
+    private String quantity;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @CreationTimestamp
+    private Instant creationTimeStamp;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Instant getCreationTimeStamp() {
+        return creationTimeStamp;
+    }
+
+    public void setCreationTimeStamp(Instant creationTimeStamp) {
+        this.creationTimeStamp = creationTimeStamp;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
 }
