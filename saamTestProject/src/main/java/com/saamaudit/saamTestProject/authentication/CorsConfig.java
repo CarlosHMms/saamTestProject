e32c40484a677bd2144fe2bd3ce86128c5094f5a
Carlos Henrique Mota Martins <107642033+CarlosHMms@users.noreply.github.com>
2025-10-30 07:29:55 -0300

    feat: add cors config
    libera o cors para consumo do nosso frontend

---
+++ saamTestProject/src/main/java/com/saamaudit/saamTestProject/authentication/CorsConfig.java
@@ -0,0 +1,32 @@
+package com.saamaudit.saamTestProject.authentication;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.addAllowedOrigin("http://localhost:5173");
+
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+        config.addAllowedMethod("OPTIONS");
+
+        config.addAllowedHeader("*");
+
+        config.setAllowCredentials(true);
+
+        source.registerCorsConfiguration("/api/v1/**", config);
+        return new CorsFilter(source);
+    }
+}
