fc4d37d553b814db81efc5e7ab273726a08a25b0
Carlos Henrique Mota Martins <107642033+CarlosHMms@users.noreply.github.com>
2025-10-29 23:59:13 -0300

    feat: create product repository

--- saamTestProject/src/main/java/com/saamaudit/saamTestProject/repositories/ProductRepository.java
+++ saamTestProject/src/main/java/com/saamaudit/saamTestProject/repositories/ProductRepository.java
@@ -1,4 +1,9 @@
 package com.saamaudit.saamTestProject.repositories;
 
-public class ProductRepository {
+import com.saamaudit.saamTestProject.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
